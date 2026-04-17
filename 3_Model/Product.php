73a93aa8885b3dc38edf09e9c8e4636ddcf60ef2
Fabiano <fabipiroli@live.com>
2026-04-17 20:20:22 -0300

    Storage Control

---
+++ 3_Model/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+class Product{
+
+    private string $name;
+    private int $gtin;
+    private float $price;
+    
+
+    public function __construct(string $name, int $gtin, float $price){
+        
+
+    }
+}
+?>
